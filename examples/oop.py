e64f1785fe8cc8a669797ea8f280ea8334205b7a
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-04-03 16:29:56 +0300

    Add property example

--- examples/oop.py
+++ examples/oop.py
@@ -3,11 +3,26 @@ from datetime import date
 
 class Person:
     count = 0  # class variable/attribute
+    MIN_YEAR = 1920
 
-    def __init__(self, name, date_of_birth):
+    def __init__(self, name, date_of_birth: date):
         self.name = name  # instance variable/attribute
         self.date_of_birth = date_of_birth
-        self.increment_count()
+        self._increment_count()
+
+    @property
+    def date_of_birth(self):
+        return self._date_of_birth
+
+    @date_of_birth.setter
+    def date_of_birth(self, value: date):
+        if value.year < self.MIN_YEAR:
+            raise ValueError(f"Invalid date of birth: {value}")
+        self._date_of_birth = value
+
+    @property
+    def age(self):
+        return self.years_since(self.date_of_birth)
 
     def __str__(self):
         return f"{self.name} ({self.date_of_birth})"
@@ -25,7 +40,7 @@ class Person:
         print(f"{greeting.capitalize()}, I am {self.name}!")
 
     @classmethod
-    def increment_count(cls):  # class method
+    def _increment_count(cls):  # class method
         cls.count += 1
 
     @staticmethod
@@ -42,6 +57,8 @@ if __name__ == "__main__":
     print(p1, repr(p1), p1.__dict__, Person.__bases__)
     p2 = Person("John", date(1984, 7, 6))
 
+    p1.name = "Jane"
+
     if p1 < p2:
         print(f"{p1.name} is younger than {p2.name}")
 
@@ -52,3 +69,14 @@ if __name__ == "__main__":
 
     print(Person.years_since(date(2024, 11, 20)))
     print(Person.years_since(date(2022, 11, 20)))
+
+    try:
+        p1.date_of_birth = date(1910, 6, 12)  # should not work
+    except ValueError as ex:
+        print(ex)
+    try:
+        p3 = Person("Jimmy", date(1910, 6, 12))
+    except ValueError as ex:
+        print(ex)
+
+    print(p1.age)
