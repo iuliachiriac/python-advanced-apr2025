b1bc8463d70d6d69c852d91e6d4fe526d9835462
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-04-03 11:14:43 +0300

    Add decorator example

---
+++ examples/decorators.py
@@ -0,0 +1,39 @@
+import functools
+
+
+def make_pretty(func):
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        print("I got decorated")
+        return func(*args, **kwargs)
+
+    return inner
+
+
+@make_pretty
+def ordinary():
+    print("I am ordinary")
+
+
+@make_pretty
+def greet(name):
+    print(f"Hello, {name}!")
+
+
+@make_pretty
+def decrement(nr, step=1):
+    """Decrements nr by step and returns the result"""
+    return nr - step
+
+
+# ordinary = make_pretty(ordinary)  # make_pretty.<locals>.inner
+ordinary()
+
+greet("Anna")
+
+result = decrement(10, step=2)
+print("Decrement result:", result)
+
+help(decrement)
+print("Function name:", decrement.__name__)
+print("Function docstring:", decrement.__doc__)
